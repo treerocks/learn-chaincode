019f88fc41592222111df2201f4af45fac138dbb
Lei Li <3stones.li@gmail.com>
2017-04-13 00:30:48 +0800

    Build pass

--- finished/chaincode_meidi.go
+++ finished/chaincode_meidi.go
@@ -59,21 +59,8 @@ type Transaction struct {
 func (t *SimpleChaincode) Init(stub shim.ChaincodeStubInterface) pb.Response {
 	fmt.Println("Chaicode_Meidi Init")
 
-	// Initialize the chaincode
-	A = args[0]
-	Aval, err = strconv.Atoi(args[1])
-	if err != nil {
-		return shim.Error("Expecting integer value for asset holding")
-	}
-	B = args[2]
-	Bval, err = strconv.Atoi(args[3])
-	if err != nil {
-		return shim.Error("Expecting integer value for asset holding")
-	}
-	fmt.Printf("Aval = %d, Bval = %d\n", Aval, Bval)
-
 	// Write the state to the ledger
-	err = stub.PutState("MeiDiFabric", []byte("This is the block chain for MeiDiBill system..."))
+	err := stub.PutState("MeiDiFabric", []byte("This is the block chain for MeiDiBill system..."))
 	if err != nil {
 		return shim.Error(err.Error())
 	}
@@ -84,76 +71,23 @@ func (t *SimpleChaincode) Init(stub shim.ChaincodeStubInterface) pb.Response {
 func (t *SimpleChaincode) Invoke(stub shim.ChaincodeStubInterface) pb.Response {
 	fmt.Println("ChainCode_Meidi Invoke")
 	function, args := stub.GetFunctionAndParameters()
-	if function == "invoke" {
-		// Make payment of X units from A to B
-		return t.invoke(stub, args)
-	} else if function == "delete" {
-		// Deletes an entity from its state
-		return t.delete(stub, args)
-	} else if function == "query" {
-		// the old "Query" is now implemtned in invoke
-		return t.query(stub, args)
+	if function == "createBill" {
+		// Create the Bill 
+		return t.createBill(stub, args)
+	} else if function == "changeBillStatus" {
+		// Change the status of Bill  
+		return t.changeBillStatus(stub, args)
+	} else if function == "queryBill" {
+		//Query the Bill info from fabric 
+		return t.queryBill(stub, args)
+	} else if function == "queryTransaction" {
+		//Query the Bill info from fabric 
+		return t.queryTransaction(stub, args)
 	}
 
 	return shim.Error("Invalid invoke function name. Expecting \"invoke\" \"delete\" \"query\"")
 }
 
-// Transaction makes payment of X units from A to B
-func (t *SimpleChaincode) invoke(stub shim.ChaincodeStubInterface, args []string) pb.Response {
-	var A, B string    // Entities
-	var Aval, Bval int // Asset holdings
-	var X int          // Transaction value
-	var err error
-
-	if len(args) != 3 {
-		return shim.Error("Incorrect number of arguments. Expecting 3")
-	}
-
-	A = args[0]
-	B = args[1]
-
-	// Get the state from the ledger
-	// TODO: will be nice to have a GetAllState call to ledger
-	Avalbytes, err := stub.GetState(A)
-	if err != nil {
-		return shim.Error("Failed to get state")
-	}
-	if Avalbytes == nil {
-		return shim.Error("Entity not found")
-	}
-	Aval, _ = strconv.Atoi(string(Avalbytes))
-
-	Bvalbytes, err := stub.GetState(B)
-	if err != nil {
-		return shim.Error("Failed to get state")
-	}
-	if Bvalbytes == nil {
-		return shim.Error("Entity not found")
-	}
-	Bval, _ = strconv.Atoi(string(Bvalbytes))
-
-	// Perform the execution
-	X, err = strconv.Atoi(args[2])
-	if err != nil {
-		return shim.Error("Invalid transaction amount, expecting a integer value")
-	}
-	Aval = Aval - X
-	Bval = Bval + X
-	fmt.Printf("Aval = %d, Bval = %d\n", Aval, Bval)
-
-	// Write the state back to the ledger
-	err = stub.PutState(A, []byte(strconv.Itoa(Aval)))
-	if err != nil {
-		return shim.Error(err.Error())
-	}
-
-	err = stub.PutState(B, []byte(strconv.Itoa(Bval)))
-	if err != nil {
-		return shim.Error(err.Error())
-	}
-
-	return shim.Success(nil)
-}
 
 // Deletes an entity from state
 func (t *SimpleChaincode) delete(stub shim.ChaincodeStubInterface, args []string) pb.Response {
@@ -172,33 +106,6 @@ func (t *SimpleChaincode) delete(stub shim.ChaincodeStubInterface, args []string
 	return shim.Success(nil)
 }
 
-// query callback representing the query of a chaincode
-func (t *SimpleChaincode) query(stub shim.ChaincodeStubInterface, args []string) pb.Response {
-	var A string // Entities
-	var err error
-
-	if len(args) != 1 {
-		return shim.Error("Incorrect number of arguments. Expecting name of the person to query")
-	}
-
-	A = args[0]
-
-	// Get the state from the ledger
-	Avalbytes, err := stub.GetState(A)
-	if err != nil {
-		jsonResp := "{\"Error\":\"Failed to get state for " + A + "\"}"
-		return shim.Error(jsonResp)
-	}
-
-	if Avalbytes == nil {
-		jsonResp := "{\"Error\":\"Nil amount for " + A + "\"}"
-		return shim.Error(jsonResp)
-	}
-
-	jsonResp := "{\"Name\":\"" + A + "\",\"Amount\":\"" + string(Avalbytes) + "\"}"
-	fmt.Printf("Query Response:%s\n", jsonResp)
-	return shim.Success(Avalbytes)
-}
 
 func (t *SimpleChaincode) createBill(stub shim.ChaincodeStubInterface, args []string) pb.Response {
 
@@ -214,15 +121,16 @@ func (t *SimpleChaincode) createBill(stub shim.ChaincodeStubInterface, args []st
 	var billtype	int
 	var form	int
 	var status	int
+	var billBytes []byte
 
-	layout := ""
-	t_issue, err =  time.parse(layout, args[4])
+	layout := "2006-01-02 15:04:05"
+	t_issue, err =  time.Parse(layout, args[4])
 	if err != nil {
 		jsonResp := "{\"Error\":\"Failed to pharse the time, plese follow the fomat as" + layout + "\"}"
 		return shim.Error(jsonResp)
 	}
 
-	t_expire, err =  time.parse(layout, args[5])
+	t_expire, err =  time.Parse(layout, args[5])
 	if err != nil {
 		jsonResp := "{\"Error\":\"Failed to pharse the time, plese follow the fomat " + layout + "\"}"
 		return shim.Error(jsonResp)
@@ -248,14 +156,105 @@ func (t *SimpleChaincode) createBill(stub shim.ChaincodeStubInterface, args []st
 		return shim.Error("Expecting integer value for asset holding")
 	}
 
-	bill = {BillId: args[0], Maker: args[1], Acceptor: args[2], Receiver: args[3],IssueDate: t_issue.Unix(), ExpireDate: t_expire.Unix() , RecBank: args[6], Amount: amount, Type: billtype, Form: form, Status: status }
+	bill = Bill{BillId:args[0], Maker: args[1], Acceptor: args[2], Receiver: args[3], IssueDate: t_issue.Unix(), ExpireDate: t_expire.Unix(), RecBank: args[6], Amount: amount, Type: billtype, Form: form, Status: status}
 
 	err = writeBill(stub, bill)
+
+	if err != nil {
+		return shim.Error("createBill(): Fail to write the bill" + err.Error())
+	}
+
+	billBytes, err = json.Marshal(&bill)
+	if err != nil {
+		return shim.Error("createBill():" + err.Error())
+	}
+
+//Any operation of Bill will treat as transaction.
+
+	transaction := Transaction{BillId: args[0], Operation: "Create", BillStatus: status, Time: time.Now().Unix(), ID: transactionNo }
+
+	err = writeTransaction(stub, transaction)
+	if err != nil {
+		return shim.Error("createBill(): Fail to write the transaction" + err.Error())
+	}
+
+	transactionNo += 1
+
+	return shim.Success(billBytes)
+
 }
 
 
 // Change the Bill status
 func (t *SimpleChaincode) changeBillStatus(stub shim.ChaincodeStubInterface, args []string) pb.Response {
+	if len(args) != 2 {
+		return shim.Error("changeBillStatus(): Incorrect number of arguments. Expecting 2")
+	}
+
+	var bill Bill
+
+	status, err := strconv.Atoi(args[1])
+	if err != nil {
+		return shim.Error("changeBillStatus(): status want Integer number")
+	}
+
+	bill,_,err = getBillById(stub, args[0])
+	if err != nil {
+
+		return shim.Error("changeBillStatus(): Fail to get Bill" + err.Error())
+
+	}
+	bill.Status = status
+
+	err = writeBill(stub, bill)
+
+	if err != nil {
+		shim.Error("changeBillStatus():fail to write Bill" + err.Error())
+	}
+
+	transaction := Transaction{BillId: args[0], Operation: "changeStatus", BillStatus: status, Time: time.Now().Unix(), ID: transactionNo }
+
+	err = writeTransaction(stub, transaction)
+	if err != nil {
+		return shim.Error("changeBillStatus(): Fail to write the transaction" + err.Error())
+	}
+
+	transactionNo += 1
+
+	return shim.Success(nil)
+}
+
+// Query Bill info 
+func (t *SimpleChaincode) queryBill(stub shim.ChaincodeStubInterface, args []string) pb.Response {
+
+	if len(args) != 1 {
+		return shim.Error("queryBill(): Incorrect number of arguments. Expecting 1")
+	}
+
+	_,billBytes,err := getBillById(stub,args[0])
+
+	if err != nil {
+		shim.Error("queryBill(): Fail to get Bill" +  err.Error())
+	}
+
+	return shim.Success(billBytes)
+
+}
+
+// Query Transaction info 
+func (t *SimpleChaincode) queryTransaction(stub shim.ChaincodeStubInterface, args []string) pb.Response {
+
+	if len(args) != 1 {
+		return shim.Error("queryTransaction(): Incorrect number of arguments. Expecting 1")
+	}
+
+	_,transBytes,err := getTransactionById(stub,args[0])
+
+	if err != nil {
+		return shim.Error("queryBill(): Fail to get Bill" +  err.Error())
+	}
+
+	return shim.Success(transBytes)
 
 }
 
@@ -292,7 +291,32 @@ func writeTransaction(stub shim.ChaincodeStubInterface, transaction Transaction)
 	return nil
 }
 
-func getBillById(stub shim.ChaincodeStubInterface, id string) (Company, []byte, error) {
+func getBillById(stub shim.ChaincodeStubInterface, id string) (Bill, []byte, error) {
+	var bill Bill
+	billBytes, err := stub.GetState("bill" + id)
+	if err != nil {
+		fmt.Println("Error retrieving cpBytes")
+	}
+	err = json.Unmarshal(billBytes, &bill)
+	if err != nil {
+		fmt.Println("Error unmarshalling Bill")
+	}
+	return bill, billBytes, nil
+}
+
+
+
+func getTransactionById(stub shim.ChaincodeStubInterface, id string) (Transaction, []byte, error) {
+	var transaction Transaction
+	transBytes, err := stub.GetState("transaction" + id)
+	if err != nil {
+		fmt.Println("Error retrieving transBytes")
+	}
+	err = json.Unmarshal(transBytes, &transaction)
+	if err != nil {
+		fmt.Println("Error unmarshalling transaction")
+	}
+	return transaction, transBytes, nil
 }
 
 func main() {
